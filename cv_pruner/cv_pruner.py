400ce4023e5b79739bf30f8ac46fd3e37a0160ac
Sigrun May <sigrun.may@gmail.com>
2022-11-25 13:52:52 +0100

    refactoring
    - add new parameters
    - bugfix
    - optimize parameters

--- cv_pruner/cv_pruner.py
+++ cv_pruner/cv_pruner.py
@@ -6,10 +6,9 @@
 
 """Pruner for hyperparameter optimization with nested cross-validation and high variance."""
 
-import math
 from enum import Enum
 from statistics import mean, median
-from typing import List, Union
+from typing import List, Optional, Union
 
 import numpy as np
 
@@ -68,7 +67,6 @@ def _extrapolate_metric(validation_metric_history: List[float], method: Method) 
     ]
 
     if len(deviation_list) > 0:
-
         if method == Method.MEAN_DEVIATION_TO_MEDIAN:
             # mean deviation in direction to optimize from median
             return median_validation_metric - mean(deviation_list)
@@ -83,30 +81,30 @@ def _extrapolate_metric(validation_metric_history: List[float], method: Method) 
 
 
 def should_prune_against_threshold(
-    current_step_of_complete_nested_cross_validation: int,
-    folds_outer_cv: int,
-    folds_inner_cv: int,
-    validation_metric_history: List[float],
-    threshold_for_pruning: float,
-    direction_to_optimize_is_minimize: bool,
-    optimal_metric: float,
-    method: Method = Method.OPTIMAL_METRIC,
+        folds_inner_cv: int,
+        validation_metric_history: List[float],
+        threshold_for_pruning: float,
+        start_step: int,
+        stop_step: int,
+        direction_to_optimize_is_minimize: bool,
+        method: Method = Method.MEAN_DEVIATION_TO_MEDIAN,
+        optimal_metric: Optional[float] = None,
 ) -> bool:
     """Pruner to detect an invalid performance evaluation value of a trial.
 
     Args:
-        current_step_of_complete_nested_cross_validation: One based step of the
-            complete nested cross-validation.
-        folds_outer_cv: Absolute number of folds for the outer cross-validation loop (one
-            based): Set to zero for standard cross-validation.
         folds_inner_cv: Absolute number of folds for the inner cross
             validation loop (one based).
         validation_metric_history: List of all previously calculated performance evaluation metric values.
         threshold_for_pruning: Threshold that should not be exceeded
             (minimizing) or fallen below (maximizing).
+        start_step: Pruning starts after a patience of the specified ``start_step`` steps. Must be greater than 2.
+            It is recommended to choose ``start_step`` greater than 3.
+        stop_step: Pruning stops after the specified ``stop_step`` steps. Must be greater than ``start_step``.
         direction_to_optimize_is_minimize: True - in case of minimizing and False - in case of maximizing.
-        optimal_metric: Optimal value for the performance evaluation metric.
         method: The extrapolation method to be used (see Method).
+        optimal_metric: Optimal value for the performance evaluation metric.
+            Must be set if extrapolation method is ``Metric.OPTIMAL_METRIC``.
 
     Returns:
         If the trial should be pruned. TRUE if it is likely that the final
@@ -114,54 +112,65 @@ def should_prune_against_threshold(
         fall below the lower threshold respectively.
         FALSE otherwise.
     """
-    current_step_inner_cv = current_step_of_complete_nested_cross_validation % folds_inner_cv
+    if not start_step > 2:
+        raise ValueError("start_step must be greater than 2!")
+    if not stop_step > start_step:
+        raise ValueError("stop_step must be greater than start_step!")
+    if method == Method.OPTIMAL_METRIC and optimal_metric is None:
+        raise ValueError("optimal_metric must be set if extrapolation method is Metric.OPTIMAL_METRIC!")
 
-    # as 0 is not defined as valid step number, step number is reset to number of inner folds
-    if current_step_inner_cv == 0:
-        current_step_inner_cv = folds_inner_cv
     prune = False
+    current_step_of_complete_nested_cross_validation = len(
+        validation_metric_history
+    )  # number of all steps s calculated so far
 
-    first_third_of_complete_nested_cross_validation = folds_outer_cv / 3
+    # consider range for pruning
+    if start_step < current_step_of_complete_nested_cross_validation <= stop_step:
 
-    # in case of standard cross-validation only one "inner-loop" is calculated
-    if folds_outer_cv == 0:
-        first_third_of_complete_nested_cross_validation = float("inf")
-
-    # starts calculating after half of the inner k-fold cross-validation of the
-    # nested cross-validation and a minimum of four steps
-    if (
-        current_step_of_complete_nested_cross_validation
-        >= math.floor(folds_inner_cv / 2)
-        < first_third_of_complete_nested_cross_validation
-        and current_step_of_complete_nested_cross_validation > 3
-    ):
         # change sign to adapt the calculations to maximize as direction to optimize
         if not direction_to_optimize_is_minimize:
             validation_metric_history = [metric * -1 for metric in validation_metric_history]
             threshold_for_pruning *= -1
 
         if method == Method.MEDIAN:
-            return median(validation_metric_history) < threshold_for_pruning
+            return threshold_for_pruning < median(validation_metric_history)
 
         # extrapolate metric for the rest of the inner cross validation loop
-        if method == Method.OPTIMAL_METRIC:
-            extrapolated_metric = optimal_metric
+        elif method == Method.OPTIMAL_METRIC:
+            extrapolated_metric = optimal_metric  # optimistically extrapolated value e
         else:
-            extrapolated_metric = _extrapolate_metric(validation_metric_history, method)
+            extrapolated_metric = _extrapolate_metric(
+                validation_metric_history, method
+            )  # optimistically extrapolated value e
 
         # extrapolate metric up to the next complete loop of the inner k-fold cross-validation  # noqa: E501
         result_already_calculated_steps = (
-            median(validation_metric_history) * current_step_of_complete_nested_cross_validation
+                median(validation_metric_history) * current_step_of_complete_nested_cross_validation
         )
-        total_steps_up_to_the_next_completed_inner_cv_loop = current_step_of_complete_nested_cross_validation + (
-            folds_inner_cv - current_step_inner_cv
+
+        #  extrapolate remaining steps of current inner cross-validation loop
+        #  no extrapolation if inner cross-validation loop is complete
+
+        # calculate number of steps up to the next completed inner cross-validation loop
+        current_step_inner_cv = current_step_of_complete_nested_cross_validation % folds_inner_cv
+        if current_step_inner_cv == 0:  # inner cross-validation loop is complete
+            remaining_steps_of_inner_cross_validation_loop = (
+                0  # no missing steps m until the next complete inner cross-validation loop
+            )
+        else:
+            remaining_steps_of_inner_cross_validation_loop = (
+                    folds_inner_cv - current_step_inner_cv
+            )  # number of missing steps m until the next complete inner cross-validation loop
+
+        total_steps_up_to_the_next_completed_inner_cv_loop = (
+                current_step_of_complete_nested_cross_validation + remaining_steps_of_inner_cross_validation_loop
         )
-        extrapolated_result_remaining_steps_of_the_inner_cv_loop = extrapolated_metric * (
-            folds_inner_cv - current_step_inner_cv
+        extrapolated_result_of_remaining_steps_of_the_inner_cv_loop = (
+                extrapolated_metric * remaining_steps_of_inner_cross_validation_loop  # type: ignore
         )
         extrapolated_result_for_next_complete_inner_cv_loop = (
-            result_already_calculated_steps + extrapolated_result_remaining_steps_of_the_inner_cv_loop
-        ) / total_steps_up_to_the_next_completed_inner_cv_loop
+                                                                      result_already_calculated_steps + extrapolated_result_of_remaining_steps_of_the_inner_cv_loop
+                                                              ) / total_steps_up_to_the_next_completed_inner_cv_loop
 
         # extrapolated results worse than threshold?
         prune = threshold_for_pruning < extrapolated_result_for_next_complete_inner_cv_loop
