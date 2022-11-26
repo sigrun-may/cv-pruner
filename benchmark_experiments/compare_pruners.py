77869e366fd37cd1bd6016aa35b8dfb62cf1a358
Sigrun May <sigrun.may@gmail.com>
2022-11-26 20:32:31 +0100

    add experiments analyses

---
+++ benchmark_experiments/compare_pruners.py
@@ -0,0 +1,174 @@
+# Copyright (c) 2021 Sigrun May, Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+# Copyright (c) 2021 Sigrun May, Ostfalia Hochschule für angewandte Wissenschaften
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""TODO: add docstring."""
+
+import numpy as np
+import pandas as pd
+
+import cv_pruner
+import plotter
+import simulate_individual_threshold_pruner as pruner_simulation
+
+NUMBER_OF_TRIALS = 40
+
+
+def percent(small, big):
+    return (np.asarray(small) / np.asarray(big)) * 100
+
+
+def fill_df(name, result, df, pruner_threshold):
+    if "steps_per_trial" in name:
+        df["steps_per_study"] = np.full(
+            len(results), pruner_simulation.RESTRICTED_NUMBER_OF_CONSIDERED_TRIALS * results[0]["steps_per_trial"]
+        )
+    elif "trimmed_mean_pruned_below_threshold_list" in name:
+        tm_below_th_value = []
+        for r in results:
+            if len(r["trimmed_mean_pruned_below_threshold_list"]) > 0:
+                tm_below_th_value.append(min(r["trimmed_mean_pruned_below_threshold_list"]))
+            else:
+                tm_below_th_value.append(pruner_threshold)
+        df["min_trimmed_mean_pruned_below_threshold"] = tm_below_th_value
+        df[name] = [len(r[name]) for r in result]
+    elif "list" in name:
+        df[name] = [len(r[name]) for r in result]
+    else:
+        df[name] = [r[name] for r in result]
+
+
+list_of_experiments = [
+    ["colon_cv_pruner", 0.5],
+    ["prostate_cv_pruner", 0.45],
+    ["leukemia_cv_pruner", 0.2],
+]
+
+experiment_count = 0
+for experiment_name, threshold in list_of_experiments:
+    experiment_count += 1
+    first = True
+    result_df_list = []
+
+    for method in cv_pruner.Method:
+        print(method.name)
+        print(experiment_name)
+        results = pruner_simulation.get_results(method, experiment_name, threshold)
+        # joblib.dump(results, "results/result_" + experiment_name + "_" + method.name + ".pkl.gz", compress=("gzip", 3))
+
+        # results = joblib.load("results/result_" + experiment_name + "-" + method.name + ".pkl.gz")
+
+        result_df = pd.DataFrame()
+        result_df["method"] = np.full(30, method.name.lower())
+        for key in results[0].keys():
+            fill_df(key, results, result_df, threshold)
+            if first and "duration_of_completed_trial_list" in key:
+                # trial duration
+                print("---")
+                durations = results[0]["duration_of_completed_trial_list"]
+                print("duration_of_completed_trials", np.sum(durations))
+                first = False
+
+        result_df["difference_best_value"] = (
+                result_df["min_trimmed_mean_pruned_below_threshold"] - result_df["study_best_value"]
+        )
+        result_df["difference_threshold"] = np.full(
+            len(result_df["min_trimmed_mean_pruned_below_threshold"]), threshold
+        ) - np.asarray(result_df["min_trimmed_mean_pruned_below_threshold"])
+
+        result_df_list.append(result_df)
+
+    # concat all methods and all studies to one experiment
+    complete_df = pd.concat(result_df_list, ignore_index=True)
+
+    complete_df.replace(
+        {
+            cv_pruner.Method.MEDIAN.name: "median only",
+            cv_pruner.Method.MEAN_DEVIATION_TO_MEDIAN.name: "mean deviation",
+            cv_pruner.Method.MAX_DEVIATION_TO_MEDIAN.name: "max deviation",
+            cv_pruner.Method.OPTIMAL_METRIC.name: "optimal metric",
+        },
+        inplace=True,
+    )
+
+    # add percentages
+    complete_df["number_of_trials_per_study"] = np.full(
+        len(complete_df["trimmed_mean_pruned_below_threshold_list"]), NUMBER_OF_TRIALS
+    )
+    complete_df["trimmed_mean_below_threshold_percent"] = 100 - percent(
+        complete_df["trimmed_mean_pruned_below_threshold_list"], complete_df["number_of_trials_per_study"]
+    )
+    complete_df["fails_standard_pruner_percent"] = 100 - percent(
+        complete_df["standard_pruner_fails_list"], complete_df["number_of_trials_per_study"]
+    )
+    complete_df["global_fails_threshold_pruner_percent"] = 100 - percent(
+        complete_df["three_layer_pruner_fails_list"], complete_df["number_of_trials_per_study"]
+    )
+    assert complete_df["steps_per_study"][0] == complete_df["number_of_trials_per_study"][0] * 300
+    complete_df["steps_three_layer_percent"] = 100 - percent(
+        complete_df["steps_three_layer_pruner"], complete_df["steps_per_study"]
+    )
+    complete_df["steps_standard_pruner_percent"] = 100 - percent(
+        complete_df["steps_standard_pruner"], complete_df["steps_per_study"]
+    )
+    assert complete_df["steps_per_study"][0] == complete_df["number_of_trials_per_study"][0] * 300
+    complete_df["steps_three_layer_percent"] = 100 - percent(
+        complete_df["steps_three_layer_pruner"], complete_df["steps_per_study"]
+    )
+    complete_df["steps_standard_pruner_percent"] = 100 - percent(
+        complete_df["steps_standard_pruner"], complete_df["steps_per_study"]
+    )
+
+    # create DataFrame for visualization
+    standard_pruner_df1 = pd.DataFrame(
+        {
+            "method": np.full(30, "ASHA"),
+            "value": complete_df["steps_standard_pruner_percent"].iloc[
+                     (30 * (experiment_count - 1)): (30 * experiment_count)
+                     ],
+            "type of comparison": np.full(30, "saved iterations"),
+        }
+    )
+    assert len(standard_pruner_df1["value"]) == 30
+    standard_pruner_df2 = pd.DataFrame(
+        {
+            "method": np.full(30, "ASHA"),
+            "value": complete_df["fails_standard_pruner_percent"].iloc[
+                     (30 * (experiment_count - 1)): (30 * experiment_count)
+                     ],
+            "type of comparison": np.full(30, "correct pruned trials\nagainst threshold"),
+        }
+    )
+    assert len(standard_pruner_df2["value"]) == 30
+    standard_pruner_df3 = pd.DataFrame(
+        {
+            "method": np.full(30, "ASHA"),
+            "value": complete_df["fails_standard_pruner_percent"].iloc[
+                     (30 * (experiment_count - 1)): (30 * experiment_count)
+                     ],
+            "type of comparison": np.full(30, "completed global best trials\nof full optimization"),
+        }
+    )
+    assert len(standard_pruner_df3["value"]) == 30
+
+    df1 = complete_df[["method", "steps_three_layer_percent"]]
+    df1.rename(columns={"steps_three_layer_percent": "value"}, inplace=True)
+    df1["type of comparison"] = "saved iterations"
+
+    df2 = complete_df[["method", "trimmed_mean_below_threshold_percent"]]
+    df2.rename(columns={"trimmed_mean_below_threshold_percent": "value"}, inplace=True)
+    df2["type of comparison"] = "correct pruned trials\nagainst threshold"
+
+    df3 = complete_df[["method", "global_fails_threshold_pruner_percent"]]
+    df3.rename(columns={"global_fails_threshold_pruner_percent": "value"}, inplace=True)
+    df3["type of comparison"] = "completed global best trials\nof full optimization"
+
+    vis_df = pd.concat(
+        [df1, df2, df3, standard_pruner_df1, standard_pruner_df2, standard_pruner_df3], ignore_index=True
+    )
+    vis_df.fillna(value=0, inplace=True)
+
+    plotter.plot_errors(complete_df, experiment_name)
+    plotter.plot_comparisons_in_percent(vis_df, experiment_name)
+    plotter.plot_stacked_pruned_trials_per_part(complete_df, experiment_name)
---
+++ benchmark_experiments/plotter.py
@@ -0,0 +1,171 @@
+# Copyright (c) 2021 Sigrun May, Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+# Copyright (c) 2021 Sigrun May, Ostfalia Hochschule für angewandte Wissenschaften
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""TODO: add docstring."""
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+NUMBER_OF_TRIALS = 40
+
+
+def get_title(experiment_name):
+    if "colon" in experiment_name:
+        title = "Colon Cancer Data"
+    elif "prostate" in experiment_name:
+        title = "Prostate Cancer Data"
+    elif "leukemia" in experiment_name:
+        title = "Leukemia Data"
+    else:
+        raise Exception("Wrong experiment")
+    return title
+
+
+def plot_errors(complete_df, experiment_name):
+    sns.set_theme(style="whitegrid")
+    sns.set_context("paper", font_scale=1.5)
+    sns.set(rc={"figure.figsize": (4, 3)})
+    g = sns.FacetGrid(complete_df, col="method")
+    g.map_dataframe(sns.histplot, x="difference_threshold", binwidth=0.0005)
+    # g.set(title='Margin of error for falsely pruned trials compared to the threshold')
+    plt.suptitle(get_title(experiment_name) + "\n")
+    plt.savefig("plots/" + experiment_name + "_hist.pdf")
+    plt.show()
+
+
+def plot_comparisons_in_percent(vis_df, experiment_name):
+    # beide Methoden (unten) funktionieren
+    # siehe auch https://seaborn.pydata.org/examples/grouped_barplot.html
+    sns.set_theme(style="whitegrid")
+    sns.set_context("paper", font_scale=1.5)
+    sns.set(rc={"figure.figsize": (11, 5)})  # width=8, height=4
+    ax = plt.gca()
+    chart = sns.barplot(x="method", y="value", hue="type of comparison", data=vis_df, ax=ax).set(
+        title=get_title(experiment_name)
+    )
+    ax.set_xticklabels(
+        labels=[
+            "median only",
+            "mean deviation\n from median",
+            "max deviation\n from median",
+            "optimal performance\n evaluation metric",
+            "ASHA pruner",
+        ],
+        rotation=45,
+    )
+    # for bars in ax.containers:
+    #     ax.bar_label(bars, fmt="%.1f")
+    # plt.tight_layout()
+    ax.set_ylabel("percent %")
+    ax.set_xlabel("\n extrapolation strategy of three-layer pruner vs standard pruning")
+
+    for p in ax.patches:
+        ax.annotate(
+            "%.2f" % p.get_height() + " %",
+            (p.get_x() + p.get_width() / 2.0, p.get_height() / 2),
+            ha="center",
+            va="center",
+            fontsize=10,
+            color="black",
+            xytext=(0, 5),
+            textcoords="offset points",
+        )
+
+    # ax = sns.catplot(kind="bar", x="method", y="value", hue="type", data=vis_df)
+
+    # plt.savefig(
+    #     "/vol/projects/smay/develop/pruner/experiments/results/"
+    #     + experiment_name
+    #     + ".pdf"
+    # )
+
+    # plt.savefig("plots/" + experiment_name + "_percent.png")
+    plt.show()
+
+
+def plot_stacked_pruned_trials_per_part(complete_df, experiment_name):
+    sns.set_theme(style="whitegrid")
+
+    # create new dataframe for visualisation
+    plot_df2 = complete_df[
+        [
+            "method",
+            "number_of_trials_pruned_by_asha_only",
+            "number_of_trials_pruned_fi",
+            "number_of_trials_pruned_threshold",
+            "number_of_unpruned_trials",
+        ]
+    ]
+    plot_df2 = plot_df2.rename(
+        columns={"number_of_trials_pruned_by_asha_only": "number of trials\npruned by ASHA only"}
+    )
+    plot_df2 = plot_df2.rename(columns={"number_of_trials_pruned_fi": "number of semantically\npruned trials"})
+    plot_df2 = plot_df2.rename(
+        columns={"number_of_trials_pruned_threshold": "number of trials\npruned against threshold"}
+    )
+    plot_df2 = plot_df2.rename(columns={"number_of_unpruned_trials": "number of unpruned trials"})
+    # print(plot_df2.to_latex(index=False))
+    grouped_df = plot_df2.groupby(by=["method"]).sum()
+
+    # # print latex table
+    # grouped_tables_for_latex(grouped_df)
+    # print(grouped_df.index)
+    # grouped_df = grouped_df.reindex(["median only", "mean deviation", "max deviation", "optimal evaluation metric"])
+    # print(grouped_df.index)
+
+    ax = grouped_df.plot.bar(
+        stacked=True,
+        color=["k", "steelblue", "lightsteelblue", "slategrey"],
+        title=get_title(experiment_name),
+        rot=45,
+        fontsize=25,
+        figsize=(14, 10),
+    )
+    ax.legend(bbox_to_anchor=(1.0, 1.0), fontsize=25)
+    # Let's put the annotations inside the bars themselves by using a
+    # negative offset.
+    y_offset = -15
+    # For each patch (basically each rectangle within the bar), add a label.
+    for bar in ax.patches:
+        ax.text(
+            # Put the next to each bar. get_x returns the start
+            bar.get_x() + bar.get_width() + bar.get_width() / 7,
+            # Vertically, add the height of the bar to the start of the bar,
+            # along with the offset.
+            bar.get_height() + bar.get_y() + y_offset,
+            # This is actual value we'll show.
+            round(bar.get_height()),
+            # Center the labels and style them a bit.
+            ha="center",
+            color="k",
+            weight="bold",
+            size=12,
+        )
+    ax.set_xticklabels(
+        labels=[
+            "median only",
+            "mean deviation\nfrom median",
+            "max deviation\nfrom median",
+            "optimal performance\nevaluation metric",
+        ],
+        rotation=45,
+    )
+    ax.set_ylabel("number of trials", fontsize=25)
+    ax.set_xlabel("\n extrapolation method the three-layer pruner", fontsize=25)
+    # ax.set_xlabel("\n combined parts of the three-layer pruner", fontsize=25)
+    # displaying the title
+    plt.title(get_title(experiment_name), fontsize=30)
+    ax.plot()
+    plt.savefig("plots/" + experiment_name + "_combined_pruner.pdf")
+    plt.show()
+
+
+def tables_for_latex(df):
+    print(df.to_latex(index=False))
+
+
+def grouped_tables_for_latex(grouped_df):
+    grouped_df.insert(loc=0, value=grouped_df.index, column="method")
+    print(grouped_df.to_latex(index=False))
