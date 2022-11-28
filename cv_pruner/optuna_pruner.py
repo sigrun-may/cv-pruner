1822a7ae7d2790b8f6d18399d513e958ebce8f0c
Sigrun May <sigrun.may@gmail.com>
2022-11-28 09:07:45 +0100

    add pruner wrapper

---
+++ cv_pruner/optuna_pruner.py
@@ -0,0 +1,28 @@
+from typing import List
+
+import optuna
+from optuna.pruners import BasePruner
+
+
+class MultiPrunerDelegate(BasePruner):
+    def __init__(self, pruner_list: List[BasePruner], prune_eager: bool = True):
+        """Initializer.
+
+        Args:
+            pruner_list: List of pruners to delegate to.
+            prune_eager: if ``True`` then ``prune`` will return as soon as one pruner returns ``True``.
+                If ``False`` then we iterate all pruners and then return ``True`` if
+                at least one of them returned ``True``.
+        """
+        self.pruner_list = pruner_list
+        self.prune_eager = prune_eager
+
+    def prune(self, study: optuna.study.Study, trial: optuna.trial.FrozenTrial) -> bool:
+        pruner_results = []
+        for pruner in self.pruner_list:
+            pruner_result = pruner.prune(study, trial)
+            if self.prune_eager and pruner_result:
+                return True
+            else:
+                pruner_results.append(pruner_result)
+        return any(pruner_results)
---
+++ tests/test_optuna_pruner.py
@@ -0,0 +1,71 @@
+from unittest.mock import MagicMock
+
+from cv_pruner.optuna_pruner import MultiPrunerDelegate
+
+
+def test_MultiPrunerDelegate_prune_false():
+    pruner_1 = MagicMock()
+    pruner_1.prune.return_value = False
+
+    pruner_2 = MagicMock()
+    pruner_2.prune.return_value = False
+
+    pruner_list = [pruner_1, pruner_2]
+
+    mpd = MultiPrunerDelegate(pruner_list=pruner_list)
+
+    prune_result = mpd.prune(None, None)
+    assert prune_result is False
+    pruner_1.prune.assert_called_once()
+    pruner_2.prune.assert_called_once()
+
+
+def test_MultiPrunerDelegate_prune_true():
+    pruner_1 = MagicMock()
+    pruner_1.prune.return_value = False
+
+    pruner_2 = MagicMock()
+    pruner_2.prune.return_value = True
+
+    pruner_list = [pruner_1, pruner_2]
+
+    mpd = MultiPrunerDelegate(pruner_list=pruner_list)
+
+    prune_result = mpd.prune(None, None)
+    assert prune_result
+    pruner_1.prune.assert_called_once()
+    pruner_2.prune.assert_called_once()
+
+
+def test_MultiPrunerDelegate_prune_true_eager():
+    pruner_1 = MagicMock()
+    pruner_1.prune.return_value = True
+
+    pruner_2 = MagicMock()
+    pruner_2.prune.return_value = False
+
+    pruner_list = [pruner_1, pruner_2]
+
+    mpd = MultiPrunerDelegate(pruner_list=pruner_list)
+
+    prune_result = mpd.prune(None, None)
+    assert prune_result
+    pruner_1.prune.assert_called_once()
+    pruner_2.prune.assert_not_called()
+
+
+def test_MultiPrunerDelegate_prune_true_not_eager():
+    pruner_1 = MagicMock()
+    pruner_1.prune.return_value = True
+
+    pruner_2 = MagicMock()
+    pruner_2.prune.return_value = False
+
+    pruner_list = [pruner_1, pruner_2]
+
+    mpd = MultiPrunerDelegate(pruner_list=pruner_list, prune_eager=False)
+
+    prune_result = mpd.prune(None, None)
+    assert prune_result
+    pruner_1.prune.assert_called_once()
+    pruner_2.prune.assert_called_once()
